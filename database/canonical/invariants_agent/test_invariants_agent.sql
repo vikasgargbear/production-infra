070a91010dd654e574698a76b57b611802e2f9e0
vikasgarg <vikasgarg304@gmail.com>
2026-08-20 10:12:10 +0530

    Align audit immutability fixture SQLSTATE

--- database/canonical/invariants_agent/test_invariants_agent.sql
+++ database/canonical/invariants_agent/test_invariants_agent.sql
@@ -151,7 +151,7 @@ BEGIN
         UPDATE core.audit_events SET summary = 'tampered'
          WHERE id = '10000000-0000-0000-0000-000000000040';
         RAISE EXCEPTION USING ERRCODE = 'P0001', MESSAGE = 'audit update was accepted';
-    EXCEPTION WHEN check_violation THEN
+    EXCEPTION WHEN integrity_constraint_violation THEN
         NULL;
     END;
 END
